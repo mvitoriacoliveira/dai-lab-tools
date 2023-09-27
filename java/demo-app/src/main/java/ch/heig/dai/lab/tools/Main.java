2495e4c4a22248a5e0386baa74d738ad0f01e0bf
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-09-27 21:49:14 +0200

    modification to demo app (inf loop + sleep)

--- java/demo-app/src/main/java/ch/heig/dai/lab/tools/Main.java
+++ java/demo-app/src/main/java/ch/heig/dai/lab/tools/Main.java
@@ -2,6 +2,12 @@ package ch.heig.dai.lab.tools;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while(true){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) { }
+
+            System.out.println("Hello world!");
+        }
     }
 }
